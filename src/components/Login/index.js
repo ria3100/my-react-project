736ef04846875480a1c16b0e314d3e87a9410467
ria <ria0130@icloud.com>
2018-05-23 18:57:42 +0900

    login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import firebase from 'firebase'
+// import firebase from 'firebase'
 
-const provider = new firebase.auth.TwitterAuthProvider()
+// const provider = new firebase.auth.TwitterAuthProvider()
 
 export default class extends React.Component {
   render() {
     return (
       <div>
-        <h2>About</h2>
-        <button onClick={this.login}>twitter</button>
+        <h2>login</h2>
+        <button>twitter</button>
       </div>
     )
   }
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -5,18 +5,13 @@ import firebase from 'firebase'
 
 import { Header, Login } from '../../components'
 
-import { Form, Icon, Input, Button } from 'antd'
-const FormItem = Form.Item
-
-function hasErrors(fieldsError) {
-  return Object.keys(fieldsError).some(field => fieldsError[field])
-}
-
 export default class extends React.Component {
   render() {
-    ;<div>
-      <h2>login</h2>
-      <Login />
-    </div>
+    return (
+      <div>
+        <h2>login</h2>
+        <Login />
+      </div>
+    )
   }
 }
