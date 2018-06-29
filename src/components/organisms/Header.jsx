77a2cf2371defc1cd7103e7654b474cbe6d8186b
ria <ria0130@icloud.com>
2018-06-29 09:57:34 +0900

    headerののborder

--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -5,17 +5,18 @@ import { Layout, Menu } from 'antd'
 const { Header } = Layout
 
 const Logo = styled.h1`
-  font-size: 1.5em;
+  width: 120px;
+  height: 63px;
   color: #fff;
+  font-size: 1.5em;
   margin: 0;
+  float: left;
 `
 
 export default class extends React.Component {
   render() {
     return <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
-        <div className="logo" style={{ float: 'left', width: '120px' }}>
-          <Logo>{this.props.title}</Logo>
-        </div>
+        <Logo>{this.props.title}</Logo>
         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} style={{ lineHeight: '64px' }}>
           <Menu.Item key="1">nav 1</Menu.Item>
           <Menu.Item key="2">nav 2</Menu.Item>
